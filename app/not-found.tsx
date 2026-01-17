85523fc9f4d855ce4542069c8b6f31be66d8b0d9
Annu Kumari <anuk35168@gmail.com>
2026-01-17 22:24:32 +0530

    added not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+
+export default function notFound(){
+    return(
+     <main className="max-w-xl mx-auto px-6 py-24 space-y-6 text-center ">
+      <h1 className="text-2xl font-medium ">Page not found</h1>
+
+      <p className="text-neutral-500">
+        The page you’re looking for doesn’t exist or was removed.
+      </p>
+
+      <div className="flex justify-center gap-4 text-sm">
+        <Link
+          href="/"
+          className="underline text-neutral-700"
+        >
+          Go home
+        </Link>
+
+        <Link
+          href="/notes"
+          className="underline text-neutral-700"
+        >
+          Read notes
+        </Link>
+      </div>
+    </main>
+
+    )
+}
